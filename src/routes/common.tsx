7877d79b1c1a360042207f66b9550e0bbf312123
chiyukikana <chiyukikana@icloud.com>
2022-09-26 20:07:59 +0800

    feat(Routes): add routes

--- src/routes/common.tsx
+++ src/routes/common.tsx
@@ -3,11 +3,31 @@ import { RouteObject } from 'react-router-dom'
 import { lazyImport } from '../utils/lazyImport'
 
 // 主页面
-const { AppPage } = lazyImport(() => import('../features/app'), 'AppPage')
+const { IndexPage } = lazyImport(() => import('../features/index'), 'IndexPage')
+
+// 内容页面
+const { LatestArchive } = lazyImport(
+  () => import('../features/archive'),
+  'LatestArchive'
+)
+const { Archive } = lazyImport(() => import('../features/archive'), 'Archive')
 
 export const commonRoutes: RouteObject[] = [
+  // 主界面
   {
     path: '/',
-    element: <AppPage />,
+    element: <IndexPage />,
+    children: [
+      // 默认显示最新的文章内容
+      {
+        index: true,
+        element: <LatestArchive />,
+      },
+      // 其他年月份的文章内容
+      {
+        path: '/archives/:year/:month',
+        element: <Archive />,
+      },
+    ],
   },
 ]
