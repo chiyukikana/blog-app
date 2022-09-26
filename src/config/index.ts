03ce13a08b1bc6c89aec150e1ec6bccfa829c6b3
chiyukikana <chiyukikana@icloud.com>
2022-09-26 20:06:49 +0800

    feat(Config): merge config.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,7 @@
-export const API_URL = './api'
+// 项目的基本路径
+export const BASE_URL = import.meta.env.VITE_BASE_URL
+
+// 项目的API路径
+export const API_URL = import.meta.env.VITE_API_URL
+
+export * from './theme'
