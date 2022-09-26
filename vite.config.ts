a62c66d380aac29480642ac1d3cad6683db5156a
chiyukikana <chiyukikana@icloud.com>
2022-09-26 20:04:03 +0800

    feat(Env): add new env vars.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname)
+  return {
+    base: env.VITE_BASE_URL,
+    plugins: [react()],
+  }
 })
