70356fcf08e4b5041404280ff966a5373719ec93
chiyukikana <chiyukikana@icloud.com>
2022-09-27 12:38:23 +0800

    perf(vite-plugin): add html template plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -6,6 +7,16 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, __dirname)
   return {
     base: env.VITE_BASE_URL,
-    plugins: [react()],
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            title: env.VITE_APP_NAME,
+          },
+        },
+        minify: false,
+      }),
+    ],
   }
 })
